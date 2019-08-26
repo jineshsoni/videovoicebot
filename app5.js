242f3d7d824e4e6436d4647304fdef7eb72455b6
Jinesh <jinesh@thinkwik.com>
2019-08-26 15:06:51 +0530

    test

--- app5.js
+++ app5.js
@@ -1,10 +1,10 @@
-const speechPitch = 1.2;
-const speechRate = 1.0;
+// const speechPitch = 1.2;
+// const speechRate = 1.0;
 const apiClient = new ApiAi.ApiAiClient({accessToken: '6b07a0d9841143c7b923a43449624bd9'});
-var msg = new SpeechSynthesisUtterance('Hi, Welcome to Toshiba. How may I help you?  Are you looking for mobile devices, memory devices or hard drives?');
+// var msg = new SpeechSynthesisUtterance('Hi, Welcome to Toshiba. How may I help you?  Are you looking for mobile devices, memory devices or hard drives?');
 var recognizedText = null;
-var voices=[];
-msg.lang='en-US';
+// var voices=[];
+// msg.lang='en-US';
 
 function isChrome() {
   var isChromium = window.chrome,
@@ -49,6 +49,18 @@ function addUserItem(text) {
   //const appContent = document.querySelector(".app-content");
   //appContent.innerHTML += '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">' + text + '<span class="msg_time_send">'+s+'</span></div><div class="img_cont_msg"><img src="img/user.png" class="rounded-circle user_img_msg"></div></div>';
   //appContent.scrollTop = appContent.scrollHeight; // scroll to bottom
+
+  console.log(text)
+  var vid = document.getElementById("myVideo");
+
+  if(text.includes('audio')){
+    vid.src = "male-audio.mp4";
+  } else {
+    vid.src = "male-detail.mp4";
+  }
+
+  
+  vid.play();
 }
 
 function displayCurrentTime() {
@@ -67,33 +79,33 @@ function addError(text) {
 function handleResponse(serverResponse) {
 
   const speech = serverResponse["result"]["fulfillment"]["speech"];
-  msg = new SpeechSynthesisUtterance(speech);
-  msg.voice = speechSynthesis.getVoices()[1];
-  //msg.voiceURI =  "Microsoft David Desktop - English (United States)";
-  //msg.name =  "Microsoft David Desktop - English (United States)";
-  msg.lang='en-US';
-  msg.rate = speechRate; 		// 0.1 to 1
-  msg.pitch = speechPitch;
-  msg.text = speech;
+  // msg = new SpeechSynthesisUtterance(speech);
+  // msg.voice = speechSynthesis.getVoices()[1];
+  // //msg.voiceURI =  "Microsoft David Desktop - English (United States)";
+  // //msg.name =  "Microsoft David Desktop - English (United States)";
+  // msg.lang='en-US';
+  // msg.rate = speechRate; 		// 0.1 to 1
+  // msg.pitch = speechPitch;
+  // msg.text = speech;
   
-  addBotItem(speech);
+  // addBotItem(speech);
   
-  msg.addEventListener("end", function(ev) {
-    //window.clearTimeout(timer);
-    //startListening();
-  });
-  msg.addEventListener("error", function(ev) {
-    //window.clearTimeout(timer);
-    //startListening();
-  });
-  msg.lang='en-US';
-  if (/webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    textSpeech(speech);
-  }
-  else if(isChrome())
-    textSpeech(speech);
-  else
-    textSpeech(speech);
+  // msg.addEventListener("end", function(ev) {
+  //   //window.clearTimeout(timer);
+  //   //startListening();
+  // });
+  // msg.addEventListener("error", function(ev) {
+  //   //window.clearTimeout(timer);
+  //   //startListening();
+  // });
+  // msg.lang='en-US';
+  // if (/webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+  //   textSpeech(speech);
+  // }
+  // else if(isChrome())
+  //   textSpeech(speech);
+  // else
+  //   textSpeech(speech);
 
 }
 function handleError(serverError) {
@@ -163,13 +175,13 @@ function testy() {
         textSpeech("Hi, Welcome to Toshiba. How may I help you?  Are you looking for mobile devices, memory devices or hard drives?");
   }*/
   
-  speech = "Hi, Welcome to Toshiba. How may I help you?";
+  // speech = "Hi, Welcome to Toshiba. How may I help you?";
 
-  addBotItem(speech);
+  // addBotItem(speech);
 
-  var timer = window.setTimeout(function() { 
-  addBotItem("Are you looking for <a id='suggest1' href='#'>mobile devices</a>, <a id='suggest3' href='#'>memory devices</a> or <a id='suggest3' href='#'>hard drives</a>?");
-  }, 4000);
+  // var timer = window.setTimeout(function() { 
+  // addBotItem("Are you looking for <a id='suggest1' href='#'>mobile devices</a>, <a id='suggest3' href='#'>memory devices</a> or <a id='suggest3' href='#'>hard drives</a>?");
+  // }, 4000);
 
   const startButton = document.querySelector("#micicon");
   
@@ -203,7 +215,7 @@ function foriPhone() {
 
   recognizedText = $('#userMsg').val();
   $('#userMsg').val('');
-  addUserItem(recognizedText);
+  // addUserItem(recognizedText);
   
   let promise = apiClient.textRequest(recognizedText);
   
@@ -221,7 +233,7 @@ var OnFinishedPlaying = function () {
 
 function textSpeech(speech){
 
-  responsiveVoice.speak(speech,"UK English Male", {onend: OnFinishedPlaying});
+  // responsiveVoice.speak(speech,"UK English Male", {onend: OnFinishedPlaying});
   /*msg1 = new SpeechSynthesisUtterance(speech);
  
     var voices=[];
